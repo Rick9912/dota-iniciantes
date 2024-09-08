76eb4b5f9048ffa5e8a7b425d49ffd9d98c3038f
Ricardo Soares <157903732+Rick9912@users.noreply.github.com>
2024-09-07 23:15:44 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,42 @@
+let herois = [
+    {
+      titulo: "Phantom Assassin",
+  descricao: "Uma assassina ágil e letal, especializada em ataques furtivos e rápidos. Seus poderes, como **Adaga Sufocante** (Stifling Dagger) e **Ataque Fantasma** (Phantom Strike), a tornam uma ameaça constante para os inimigos. **Dica:** Utilize Adaga Sufocante para silenciar inimigos e Ataque Fantasma para eliminar alvos prioritários. É um herói que exige habilidade para maximizar seu potencial.",
+  link: "https://www.youtube.com/watch?v=xDAoJnlKZ40",
+  tags: "agi melee assassin phantom strike blur coup de grace pa PA pa"
+
+     },
+     {
+      titulo: "Drow Ranger",
+      descricao: "Uma elfa arqueira especializada em ataques de longo alcance e controle de multidão. Seus poderes, como **Seta Congelante** (Frost Arrows) e **Rajada** (Gust), a tornam uma excelente atiradora e perseguidora. **Dica:** Utilize Seta Congelante para desacelerar inimigos e criar oportunidades para seus aliados. Rajada é ideal para empurrar linhas e escapar de ganks.",
+      link: "https://www.youtube.com/watch?v=dqFRZucnp1I",
+      tags: "agi range markmanship frost arrows"
+     },
+     {
+      titulo: "Axe",
+      descricao: "Um bárbaro resistente e feroz, mestre em controle de multidão e dano em área. Seus poderes, como **Lâmina de Culto** (Culling Blade) e **Fome de Batalha** (Battle Hunger), o tornam um iniciador formidável. **Dica:** Utilize Lâmina de Culto para finalizar inimigos com pouca vida e Fome de Batalha para aumentar sua força e regeneração. É um excelente herói para proteger seus aliados.",
+      link: "https://www.youtube.com/watch?v=pBLSXy40hds",
+      tags: "strength tank culling blade battle hunger"
+     },
+     {
+      titulo: "Juggernaut",
+    descricao: "Um guerreiro ágil e letal, mestre em esquivar ataques e causar dano físico massivo. Seus poderes, como **Blade Fury** e **Healing Ward**, o tornam um duelista imparável. **Dica:** Utilize Blade Fury para cortar seus inimigos em pedaços e Healing Ward para se manter vivo em meio à batalha.",
+    link: "https://www.youtube.com/watch?v=IwiTC5_oihQ",
+    tags: "agi melee blade_fury healing_ward jugger"
+     },
+     
+       {
+      titulo: "Legion Commander",
+    descricao: "Uma guerreira implacável que busca a glória na batalha. Seus poderes, como **Desafio** (Duel) e **Presságio da Morte** (Moment of Courage), a tornam uma duelista formidável e uma líder inspiradora. **Dica:** Utilize Desafio para eliminar alvos prioritários e Moment of Courage para aumentar sua força e armadura.",
+    link: "https://www.youtube.com/watch?v=Mjtl9voucUQ",
+    tags: "strength melee duel moment_of_courage"
+     },
+     {
+      titulo: "Chaos Knight",
+    descricao: "Um cavaleiro corrompido com a habilidade de invocar criaturas das trevas. Seus poderes, como **Chaos Bolt** e **Reality Rift**, o tornam um mago versátil e poderoso. **Dica:** Utilize Chaos Bolt para causar dano mágico em área e Reality Rift para escapar do perigo ou iniciar uma luta.",
+    link: "https://www.youtube.com/watch?v=6GerA0wof08",
+    tags: "strength int melee chaos_bolt reality_rift CK"
+     }
+
+];
+
